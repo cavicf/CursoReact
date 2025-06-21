e6aa37357a3201f9af9896834a76f29f9659f658
Camily <cavicfina@gmail.com>
2025-06-21 19:31:54 -0300

    Entendo props drilling

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,18 +1,31 @@
 import { Card } from "./components/card";
 import React from "react";
+import { Header } from "./components/Header";
+import { Footer } from "./components/footer";
 
 function App() {
   return (
     <>
+    <Header/>
       <div>
         Hello World
-        <Card/>
-        <Card/>
-        <Card/>
+        <Card 
+          id={2}
+          paragrafo="Typescript"
+          details="TS para frontend e backend"
+        />
+        <Card 
+          id={3}
+          paragrafo="HTML"
+          details="HTML para frontend"
+        />
+        <Card 
+          id={4}
+          paragrafo="SQL"
+          details="SQL para banco de dados"
+        />
       </div>
-      <div>
-        Footer
-      </div>
+      <Footer/>
     </>
   );
 }
--- my-app/src/components/card.tsx
+++ my-app/src/components/card.tsx
@@ -1,9 +1,15 @@
-export const Card = () => {
+interface ICard{
+    id: number,
+    paragrafo: string,
+    details: string
+}
+
+export const Card = ({id, paragrafo, details}: ICard) => {
     return(
          <div>
-            <h1>Card</h1>
-            <p>Paragrafo</p>
-            <p>Detalhes sobre o card </p>
+            <h1>Card {id}</h1>
+            <p>{paragrafo}</p>
+            <p>{details}</p>
         </div>
     );  
 }
