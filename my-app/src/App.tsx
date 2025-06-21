e0c3235291a4f415c2fb3b25f14f2dc95982be0e
Camily <cavicfina@gmail.com>
2025-06-21 19:03:24 -0300

    Iniciando estudos de React

---
+++ my-app/src/App.tsx
@@ -0,0 +1,20 @@
+import { Card } from "./components/card";
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        Hello World
+        <Card/>
+        <Card/>
+        <Card/>
+      </div>
+      <div>
+        Footer
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/card.tsx
@@ -0,0 +1,9 @@
+export const Card = () => {
+    return(
+         <div>
+            <h1>Card</h1>
+            <p>Paragrafo</p>
+            <p>Detalhes sobre o card </p>
+        </div>
+    );  
+}
